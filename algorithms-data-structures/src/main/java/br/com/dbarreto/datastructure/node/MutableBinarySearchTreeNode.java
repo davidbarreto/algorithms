d7093a685e86d7b77eda99636bb136e539476b8e
David Barreto <david.brto@gmail.com>
2025-12-23 00:33:17 +0000

    Fix compilation error

--- algorithms-data-structures/src/main/java/br/com/dbarreto/datastructure/node/MutableBinarySearchTreeNode.java
+++ algorithms-data-structures/src/main/java/br/com/dbarreto/datastructure/node/MutableBinarySearchTreeNode.java
@@ -1,4 +1,15 @@
 package br.com.dbarreto.datastructure.node;
 
 public interface MutableBinarySearchTreeNode<T extends Comparable<T>, N extends MutableBinarySearchTreeNode<T,N>> extends BinarySearchTreeNode<T>, MutableBinaryTreeNode<T, N> {
+
+    @Override
+    default N left() {
+        return leftMutable();
+    }
+
+    @Override
+    default N right() {
+        return rightMutable();
+    }
 }
+
