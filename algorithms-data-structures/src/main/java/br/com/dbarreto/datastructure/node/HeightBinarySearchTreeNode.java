3adf5c60165162fc27541849b9c49be2fd821b4e
David Barreto <david.brto@gmail.com>
2025-12-30 16:32:39 +0000

    Add Override annotation

--- algorithms-data-structures/src/main/java/br/com/dbarreto/datastructure/node/HeightBinarySearchTreeNode.java
+++ algorithms-data-structures/src/main/java/br/com/dbarreto/datastructure/node/HeightBinarySearchTreeNode.java
@@ -1,5 +1,6 @@
 package br.com.dbarreto.datastructure.node;
 
 public interface HeightBinarySearchTreeNode<T extends Comparable<T>> extends BinarySearchTreeNode<T> {
+    @Override
     int height();
 }
