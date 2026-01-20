86d6948f5559c2f3d0f7b231f8b30e2d649e8645
David Barreto <david.brto@gmail.com>
2026-01-20 22:19:38 +0000

    Remove references to implementation in Tuple interface

--- algorithms-data-structures/src/main/java/br/com/dbarreto/datastructure/tuple/Tuple.java
+++ algorithms-data-structures/src/main/java/br/com/dbarreto/datastructure/tuple/Tuple.java
@@ -1,15 +1,13 @@
 package br.com.dbarreto.datastructure.tuple;
 
-import br.com.dbarreto.datastructure.tuple.impl.RelativeTuple;
-
 public interface Tuple<T> extends Iterable<T> {
 
     T get(int index);
-    RelativeTuple<T> slice(String expression);
-    RelativeTuple<T> sliceFrom(int from);
-    RelativeTuple<T> sliceTo(int to);
-    RelativeTuple<T> slice(int from, int to);
-    RelativeTuple<T> reverse();
+    Tuple<T> slice(String expression);
+    Tuple<T> sliceFrom(int from);
+    Tuple<T> sliceTo(int to);
+    Tuple<T> slice(int from, int to);
+    Tuple<T> reverse();
     int size();
     default boolean isEmpty() {
         return size() == 0;
